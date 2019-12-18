51c83bbd8c5ab3b91d5073b59e93faa2ac9e028e
zefrof <zefrof@protonmail.com>
2019-12-17 20:14:25 -0500

    basic start

---
+++ app/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+@app.route('/deck/')
+@app.route('/deck/<id>')
+def deck(id = None):
+    return render_template('deck.html', id = id)
