ee507c0b90e74e76cc8f9962faa53074fa46c839
Darren <zefrof@protonmail.com>
2019-12-19 15:42:05 -0500

    opening file

--- app/cardFetch.py
+++ app/cardFetch.py
@@ -1,27 +1,7 @@
 import threading, time, requests
+from classes import Thread
 
 threadLimiter = threading.BoundedSemaphore(4)
-
-class Thread(threading.Thread):
-    def __init__(self, threadID, name, counter):
-        threading.Thread.__init__(self)
-        self.threadID = threadID
-        self.name = name
-        self.counter = counter
-    def run(self):
-        threadLimiter.acquire()
-
-        print("Starting " + self.name)
-        print_time(self.name, 5, self.counter)
-        print("Exiting " + self.name)
-
-        threadLimiter.release()
-
-def print_time(threadName, counter, delay):
-    while counter:
-        time.sleep(delay)
-        print("%s: %s" % (threadName, time.ctime(time.time())))
-        counter -= 1
 
 def download_file(url):
     local_filename = url.split('/')[-1]
@@ -38,13 +18,17 @@ def download_file(url):
 
 def main():
 
-    cardData = download_file("https://archive.scryfall.com/json/scryfall-default-cards.json")
-
-    print(cardData)
-
+    bulkFile = download_file("https://archive.scryfall.com/json/scryfall-default-cards.json")
     threads = []
 
+    try:
+        f = open(bulkFile, "r")
+    except:
+        print("!!! Opening the file failed")
+
     for i in range(0, 10):
+
+
         threadX = Thread(i, "Thread-" + str(i), i)
         threads.append(threadX)
         threadX.start()
--- app/classes.py
+++ app/classes.py
@@ -1,3 +1,27 @@
+class Thread(threading.Thread):
+    def __init__(self, threadID, name, counter, json):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):
+        threadLimiter.acquire()
+
+        print("Starting " + self.name)
+
+
+
+
+        print("Exiting " + self.name)
+
+        threadLimiter.release()
+
+def print_time(threadName, counter, delay):
+    while counter:
+        time.sleep(delay)
+        print("%s: %s" % (threadName, time.ctime(time.time())))
+        counter -= 1
+
 class card:
     name = ""
     manaCost = ""
