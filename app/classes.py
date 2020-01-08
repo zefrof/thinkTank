6f5411942b87dbefd53b82ddd4987542b9e412e5
Darren <zefrof@protonmail.com>
2020-01-08 16:25:48 -0500

    class is in session

--- app/classes.py
+++ app/classes.py
@@ -10,33 +10,63 @@ class Thread(threading.Thread):
     def run(self):
         threadLimiter.acquire()
 
-        print(self.json['name'])
+        #print(self.json['name'])
+
+        card = Card()
+        card.setCard(self.json)
 
         threadLimiter.release()
 
-    def printJson(self, threadName, counter, delay, json):
-        while counter:
-            time.sleep(delay)
-            print("%s: %s - %s" % (threadName, time.ctime(time.time()), json))
-            counter -= 1
-
-class card:
+class Card:
+    scryfallId = ""
+    tcgplayerId = 0
     name = ""
+    relaseDate = ""
+    layout = ""
     manaCost = ""
     cmc = 0
     typeLine = ""
     oracleText = ""
+    flavorText = ""
+    typeLine = ""
+    power = ""
+    toughness = ""
+    colors = []
+    colorIden = []
+    legalities = []
+    reserve = 0
+    foil = 0
+    nonfoil = 0
+    oversized = 0
+    promo = 0
+    reprint = 0
+    variation = 0
+    mtgSet = ""
+    setCode = ""
+    collectorNumber = ""
+    rarity = ""
+    watermark = ""
+    curPrice = ""
 
     def __init__(self):
         pass
 
-class deck:
+    def setCard(self, json):
+        print(json)
+
+    def getCard(self):
+        pass
+
+    def commitCard(self):
+        pass
+
+class Deck:
     name = ""
     archetype = ""
     pilot = ""
     finish = 0
 
-class event:
+class Event:
     name = ""
     date = ""
     location = ""
