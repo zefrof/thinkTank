0f9ffe945720ddb82fe44ae02894869373a1b858
Darren <zefrof@protonmail.com>
2020-01-24 15:47:29 -0500

    eventFetch outline

--- app/eventFetch.py
+++ app/eventFetch.py
@@ -6,8 +6,19 @@ def main():
     errCount = 0
     url = "https://www.mtgtop8.com/event?e="
 
+    index = 0
     while errCount < 4:
+        page = requests.get(url + str(index))
 
+        if(page.status_code == 404):
+            errCount += 1
+            break
+
+        text = BeautifulSoup(page.text, "html.parser")
+
+        print(text)
+
+        index += 1
 
 
 
